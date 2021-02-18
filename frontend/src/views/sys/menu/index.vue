ed006cc4182e0ac3f18dccbf351546dcd025f469
qzq <472597709@qq.com>
2021-02-18 15:20:28 +0800

    完善模板

--- frontend/src/views/sys/menu/index.vue
+++ frontend/src/views/sys/menu/index.vue
@@ -1,11 +1,70 @@
 <template>
-  <div class="app-container">menu</div>
+  <div class="app-container">
+    <el-form :inline="true" :model="search" class="demo-form-inline">
+      <el-form-item label="账号">
+        <el-input v-model="search.user" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="index" width="50"> </el-table-column>
+      <el-table-column prop="date" label="账号"> </el-table-column>
+      <el-table-column prop="date" label="角色"> </el-table-column>
+      <el-table-column prop="name" label="状态"> </el-table-column>
+      <el-table-column prop="date" label="创建时间"> </el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template>
+          <el-button type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      currentPage: 1,
+      search: {
+        user: "",
+      },
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
   },
   methods: {},
 };
