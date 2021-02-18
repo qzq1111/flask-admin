e91474ce192c8c20014ff368dd58d6e3e0230d15
qzq <472597709@qq.com>
2021-02-18 18:59:15 +0800

    添加用户新增界面

--- frontend/src/views/sys/user/index.vue
+++ frontend/src/views/sys/user/index.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="app-container">
-    <el-form
-      :inline="true"
-      :model="search"
-      class="demo-form-inline"
-      size="small"
-    >
-      <el-form-item label="账号">
-        <el-input
-          v-model="search.user_name"
-          placeholder="账号"
-          clearable
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
+    <el-row>
+      <el-form
+        :inline="true"
+        :model="search"
+        class="demo-form-inline"
+        size="small"
+      >
+        <el-form-item label="账号">
+          <el-input v-model="search.user_name" placeholder="账号" clearable />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSearch">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </el-row>
+    <el-row>
+      <el-col style="text-align: right" :span="22">
+        <el-button type="primary" size="small" @click="onAddUser"
+          >新增</el-button
+        >
+      </el-col>
+    </el-row>
 
     <el-table
+      v-loading="listLoading"
       :data="userList"
       style="width: 100%"
-      v-loading="listLoading"
       element-loading-text="Loading"
     >
       <el-table-column type="index" width="50" label="#"> </el-table-column>
@@ -56,15 +61,24 @@
       :total="total"
     >
     </el-pagination>
+    <userTemplate
+      v-if="userVisible"
+      :visible="userVisible"
+      @close="userVisible = false"
+    ></userTemplate>
   </div>
 </template>
 
 <script>
 import { getUserList, changeUserStatus } from "@/api/user";
-
+import userTemplate from "./userTemplate";
 export default {
+  components: {
+    userTemplate,
+  },
   data() {
     return {
+      userVisible: false,
       listLoading: true,
       total: 0,
       search: {
@@ -79,11 +93,14 @@ export default {
     this.fetchUserListData();
   },
   methods: {
+    onAddUser() {
+      this.userVisible = true;
+    },
     handleStatusChange(row) {
       console.log(row);
       let text = row.status === 1 ? "启用" : "禁用";
       this.$confirm(
-        '确认要' + text + ':' + row.user_name + ' 用户吗?',
+        "确认要" + text + ":" + row.user_name + " 用户吗?",
         "警告",
         {
           confirmButtonText: "确定",
@@ -102,7 +119,7 @@ export default {
         });
     },
 
-    onSubmit() {
+    onSearch() {
       this.search.page = 1;
       this.fetchUserListData();
     },
